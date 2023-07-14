5d735bcdc74b66103ccfc57d0720037eaea7da6e
eshiyu park <eshiyupark@gmail.com>
2023-07-15 02:40:52 +1000

    Implementing handleSubmit function to add items to list. NOT COMPLETE

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import "./styles.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, "hello"
+      ]
+    })
+  }
+
+  console.log(todos)
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Task</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
