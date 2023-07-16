57d0c9a0d3131f890b780274fb60a5f30fdeff00
eshiyu park <eshiyupark@gmail.com>
2023-07-16 19:30:25 -0400

    Added function toggleTodo for completion checkbox. Added function deleteTodo for delete button

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,23 @@ export default function App() {
     setNewItem("")
   }
 
-  console.log(todos)
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
@@ -34,10 +50,10 @@ export default function App() {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed}/>
+                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete Task</button>
+              <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete Task</button>
             </li>
           )
         })}
